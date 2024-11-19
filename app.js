2c46249d985f9a300782464f308025738c7b86f9
Fast Junkie <fast-junkie@pm.me>
2024-11-19 10:58:42 -0500

    Completed module-02...

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 import express from 'express';
+import { readData } from './helpers.js';
 
 const app = express();
+
+// GET /patients
+app.get('/patients', async (req, res) => {
+  try {
+    const results = await readData();
+    res.status(200).json(results);
+  } catch (err) {
+    console.debug('Error: %o', err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// GET /patients/:id
+app.get('/patients/:id', async (req, res) => {
+  try {
+    const results = await readData();
+    const { id } = req.params;
+    const patient = results.find((p) => p.id === id);
+    if (!patient) {
+      res.status(404).json({ error: `Patient not found for id: ${id}` });
+    }
+    res.status(200).json(patient);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST /patients
+// PUT /patients/:id
+// DELETE /patients/:id
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
